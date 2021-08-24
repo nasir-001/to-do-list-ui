7309ad581ebd700e085888eb6a65de0764e3fc3d
Nasir <nasirlawal001@gmail.com>
2021-08-24 19:37:34 +0100

    first commit

---
+++ src/layouts/Index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="tw-mt-80 tw-text-2xl">
+    index
+  </div>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Index'
+})
+</script>
